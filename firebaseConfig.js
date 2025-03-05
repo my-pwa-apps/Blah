24657d78b6a1606bec61aae66c628bd723ee4412
GarfieldApp <garfieldapp@outlook.com>
2025-03-05 12:24:16 +0100

    fixes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBHO9OHKZOROqQysmpR6w5wPtQutb1lMvY",
+    authDomain: "blah-fb162.firebaseapp.com",
+    databaseURL: "https://blah-fb162-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "blah-fb162",
+    storageBucket: "blah-fb162.firebasestorage.app",
+    messagingSenderId: "296240607863",
+    appId: "1:296240607863:web:076999ab068e03a41c8c72"
 };
--- scripts.js
+++ scripts.js
@@ -1,19 +1,8 @@
+import { firebaseConfig } from './firebaseConfig.js';
 import { loadDiscussions } from './discussionHandler.js';
 
-// Firebase configuration
-var firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-const db = firebase.database();
 
 document.getElementById('allDiscussionsBtn').addEventListener('click', () => {
     loadDiscussions('all');
@@ -21,4 +10,12 @@ document.getElementById('allDiscussionsBtn').addEventListener('click', () => {
 
 document.getElementById('friendsDiscussionsBtn').addEventListener('click', () => {
     loadDiscussions('friends');
+});
+
+document.getElementById('darkModeToggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelectorAll('button').forEach(button => {
+        button.classList.toggle('dark-mode');
+    });
 });
